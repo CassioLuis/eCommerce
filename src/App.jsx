309db6aa3a854046a6b4d2b41bef8118ad76370b
CassioLuis <cassiocaruzo@gmail.com>
2022-06-02 17:49:40 -0300

    alteracoes carrinho

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,91 @@
-import './App.css'
+import "./App.css";
+import Cardcart from "./cardCart.jsx";
 
 const navBar = () => {
   return (
     <div className="h-full flex flex-col">
-      <nav className="navegacao bg-gray-400 h-20 flex center items-center justify-between px-5 grid-cols-full">
+      <nav className="bg-gray-400 h-[10vh] flex center items-center justify-between px-5 grid-cols-full">
         <div className="flex">
-          <button className="bg-gray-300 rounded flex content-center mx-2 px-4 py-2">
+          <button className="drop-shadow-md bg-gray-300 rounded flex content-center mx-2 px-4 py-2">
             Botao
           </button>
-          <button className="bg-gray-300 rounded flex content-center mx-2 px-4 py-2">
+          <button className="drop-shadow-md bg-gray-300 rounded flex content-center mx-2 px-4 py-2">
             Botao
           </button>
-          <button className="bg-gray-300 rounded flex content-center mx-2 px-4 py-2">
+          <button className="drop-shadow-md bg-gray-300 rounded flex content-center mx-2 px-4 py-2">
             Botao
           </button>
         </div>
         <div className="flex">
-          <button className="bg-gray-300 rounded-full flex content-center mx-2 px-4 py-2">
+          <button className="drop-shadow-md bg-gray-300 rounded-full flex content-center mx-2 px-4 py-2">
             a
           </button>
         </div>
       </nav>
-      <div className="h-full bg-gray-100 flex">
-        <div className="flex flex-col w-4/6">
-          <header className="flex items-center bg-gray-300 px-10 py-5 h-20 w-full">
+      <div className="h-[90vh] bg-gray-100 flex">
+        <div className="flex flex-col w-[70vw]">
+          <header className="flex items-center bg-gray-300 px-10 py-5 h-[10vh] w-full">
             Produtos
           </header>
-          <main className="b g-gray-200 h-full w-full py-5 px-10">asdasd</main>
+          <main className="b g-gray-200 h-[90vh] w-full py-5 px-10">
+            asdasd
+          </main>
         </div>
-        <aside className="bg-gray-200 h-full w-2/6 py-5 px-5 -mr-[100%]">asdasd</aside>
+        <aside className="flex bg-gray-200 h-full w-[30vw] flex-col">
+          <div className="h-[80vh] overflow-auto p-6">
+            <div className="flex justify-between">
+              <h2>Carrinho</h2>
+              <button>X</button>
+            </div>
+            <Cardcart />
+            <Cardcart />
+            <Cardcart />
+            <Cardcart />
+            <Cardcart />
+            <Cardcart />
+            <Cardcart />
+            <Cardcart />
+            <Cardcart />
+            <Cardcart />
+            <Cardcart />
+            <Cardcart />
+            <Cardcart />
+            <Cardcart />
+            <Cardcart />
+            <Cardcart />
+            <Cardcart />
+            <Cardcart />
+            <Cardcart />
+          </div>
+          <div className="h-[20vh] border-t-2 border-indigo-500 p-6 flex">
+            <div className="w-full flex flex-col justify-between">
+              <div className="flex justify-between">
+                <p className="font-bold">Subtotal</p>
+                <p className="font-bold">R$ 500,00</p>
+              </div>
+              <p className="text-sm text-gray text-top -mt-2">
+                Frete e impostos calculados no fechamento da compra
+              </p>
+              <button className="bg-gray-600 text-white font-bold w-full p-1">
+                Concluir Compra
+              </button>
+              <p className="text-sm text-gray text-center">
+                ou Continuar Comprando ->
+              </p>
+            </div>
+          </div>
+        </aside>
       </div>
     </div>
   );
 };
 
 export default navBar;
+
+// 494,66 x 961 com varios
+// 478,66 x 787,5 com varios
+// 461,66 x 200
+
+// 500,33 x 887,14 com poucos
+// 484,33 x 725,94 com poucos
+// 467,33 x 200
---
+++ src/cardCart.jsx
@@ -0,0 +1,16 @@
+
+ const cardCart = ()=>{
+  return (
+    <div className="h-[18vh] w-full flex border-b-2 border-indigo-500 py-6">
+      <div className="rounded bg-gray-200 h-full w-48 border-2 border-indigo-500"></div>
+      <div className="w-full h-full flex pl-1 flex-col justify-between">
+        <div className="rounded-lg bg-gray-200 w-full px-3 py-1.5">a</div>
+        <div className="rounded-lg bg-gray-200 w-full px-3 py-1.5">a</div>
+        <div className="rounded-lg bg-gray-200 w-full px-3 py-1.5">a</div>
+        <div className="rounded-lg bg-gray-200 w-full px-3 py-1.5">a</div>
+      </div>
+    </div>
+  );
+};
+
+export default cardCart;;
